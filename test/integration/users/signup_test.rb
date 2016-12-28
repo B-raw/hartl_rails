25c650ae420a4c727587cc52f1aa5ac1fd6d3260
B-raw <michaelbarry1989@hotmail.com>
2016-12-28 10:28:20 +0000

    Finish user signup

---
+++ test/integration/users/signup_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid form info on signup does not create user" do
+    get signup_path
+    assert_select "form[action='/signup']"
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+  end
+
+  test "invalid form gives correct error messages" do
+    get signup_path
+    assert_select "form[action='/signup']"
+    post users_path, params: { user: { name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    assert_select "div.alert", "The form contains 4 errors."
+    assert_select "ul>li:nth-of-type(1)", "Name can't be blank"
+    assert_select "ul>li:nth-of-type(2)", "Email is invalid"
+    assert_select "ul>li:nth-of-type(3)", "Password is too short (minimum is 6 characters)"
+    assert_select "ul>li:nth-of-type(4)", "Password confirmation doesn't match Password"
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid user is saved" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "batman",
+                                         email: "batman@gotham.com",
+                                         password: "foobar",
+                                         password_confirmation: "foobar"}}
+      follow_redirect!
+      assert_template 'users/show'
+      assert_not flash.empty?
+    end
+  end
+end
